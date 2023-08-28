d957fbd512a1216ebd57327ba16676f46605432c
KishanSingh <123284392+kishansingh-AWS@users.noreply.github.com>
2023-08-28 13:47:40 +0530

    Update App.java

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello,this is kishan!!!!!!!!!!" );
+        System.out.println( "Hello,this is a class where we are learning cs" );
     }
 }
